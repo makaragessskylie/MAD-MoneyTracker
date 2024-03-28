20cc739b74f93aef773cf38f17956f8c6a50eb9c
makaragessskylie <makarageskyliee@gmail.com>
2024-03-28 09:30:34 +0800

    adding react navigation and refactor router, pages

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import SplashScreen from './src/pages/SplashScreen'
+import React from 'react';
+import SplashScreen from './src/pages/SplashScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import Router from './src/router';
 
 const App = () => {
-  return <SplashScreen />;
-}
+  return (
+    <NavigationContainer>
+      <Router />
+    </NavigationContainer>
+  );
+};
 
-export default App
+export default App;
 
--- src/pages/SplashScreen/index.tsx
+++ src/pages/SplashScreen/index.tsx
@@ -1,28 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Logo from '../../assets/icon/Frame.svg'
+import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect} from 'react';
+import Logo from '../../assets/icon/Frame.svg';
 
-const SplashScreen = () => {
+
+const SplashScreen = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.replace('SignIn');
+    }, 3000);
+  }, []);
+
   return (
     <View style={styles.container}>
       <Logo />
-      <Text style={styles.text}>MoneyTracker</Text>
+      <Text style={styles.text}>Money Tracker</Text>
     </View>
   );
 };
-
-
-export default SplashScreen
-
+export default SplashScreen;
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#02CF8E',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    text: {
-      fontSize: 32,
-      fontFamily: 'poppins-Medium',
-    },
+  container: {
+    backgroundColor: '#02CF8E',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 32,
+    fontFamily: 'Poppins-Medium',
+  },
 });
